22e2000876ad92858754d76ceecb76468d8db0b5
gdmich <59811223+gdmich@users.noreply.github.com>
2025-04-29 22:33:22 +0300

    Add round logic

--- logic.js
+++ logic.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
 
     let roll = Math.floor(Math.random() * 3)
@@ -26,14 +29,51 @@ function getHumanChoice(){
 
 }
 
-function humanScore(){
+function playRound(humanChoice, computerChoice){
 
-    let humScore = 0;
+    humanChoice = humanChoice.toLowerCase();
 
-}
+    if(humanChoice === computerChoice){
 
-function computerScore(){
+        return "Draw!";
 
-    let pcScore = 0;
-    
+    }
+
+    if(humanChoice === "rock"){
+        if(computerChoice === "paper"){
+
+            computerScore++;
+            return "You lose! Paper beats rock.";
+        }
+        else if(computerChoice === "scissors"){
+
+            humanScore++;
+            return "You win! Rock beats scissors."
+        }
+    }
+    else if(humanChoice === "paper"){
+        if(computerChoice === "rock"){
+
+            humanScore++;
+            return "You win! Paper beats rock."
+        }
+        else if(computerChoice === "scissors"){
+
+            computerScore++;
+            return "You lose! Scissors beats paper."
+        }
+    }
+    else if(humanChoice === "scissors"){
+        if(computerChoice === "rock"){
+
+            computerScore++;
+            return "You lose! Rock beats scissors."
+        }
+        else if(computerChoice === "paper"){
+
+            humanScore++;
+            return "You win! Scissors beats paper."
+        }
+    }
+
 }
